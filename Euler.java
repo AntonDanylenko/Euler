3e138f77803cd0a04109a9cca52021a1ec7d38b2
AntonDanylenko <adanylenko@stuy.edu>
2018-03-09 15:28:45 -0500

    finished prob1, prob2, started prob3

--- Euler.java
+++ Euler.java
@@ -1,3 +1,50 @@
 public class Euler{
-    
+    public static int prob1(int maxVal){
+	int result = 0;
+	for (int n=1; n<maxVal; n++){
+	    if (n%3 == 0 || n%5 == 0){
+		result += n;
+	    }
+	}
+	return result;
+    }
+
+
+
+    public static int prob2(int maxVal){
+	int result = 0;
+	int m = 0;
+	int n = 1;
+	while (n<maxVal){
+	    if (n%2 == 0){
+		result += n;
+	    }
+	    int temp = n;
+	    n += m;
+	    m = temp;
+	}
+	return result;
+    }
+
+
+
+    public static int prob3(int Val){
+	int result = 0;
+	int newNum = Val;
+	int maxPrime = 2;
+	int prime = 2;
+	while (newNum>1){
+	    newNum = newNum/prime;
+	    //prime += ?;
+	}
+	return maxPrime;
+    }	    
+
+
+
+    public static void main(String[]args){
+	//System.out.println("problem1: " + prob1(1000));
+	//System.out.println("problem2: " + prob2(4000000));
+	System.out.println("problem3: " + prob3(600851475143));
+    }
 }
